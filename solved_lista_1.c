9e1a041906c62b7cd3fd5d560d93de08418494be
juanmanoel <37448831+juanengml@users.noreply.github.com>
2019-03-12 08:24:57 -0300

    Create solved_lista_1.c

---
+++ solved_lista_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void saida(int idade,char name[]){
+  printf("%d: %s\n",idade,name);
+}
+
+
+void main(){
+//  saida(10,"Juan");
+
+}
